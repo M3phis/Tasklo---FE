bea35d18d22abb48f4e268ee454be79f8a94b87f
Tomer <tomera.almog9@gmail.com>
2025-06-21 20:28:36 +0300

    icon aliognments

--- src/cmps/TaskDetails.jsx
+++ src/cmps/TaskDetails.jsx
@@ -654,17 +654,25 @@ export function TaskDetails({}) {
 
             <div className="task-details-description">
               <div className="description-header">
-                <svg
-                  className="description-icon"
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="20px"
-                  viewBox="0 -960 960 960"
-                  width="20px"
-                  fill="#5f6368"
+                <div className="description-header-left">
+                  <svg
+                    className="description-icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="20px"
+                    viewBox="0 -960 960 960"
+                    width="20px"
+                    fill="#5f6368"
+                  >
+                    <path d="M160-200v-80h400v80H160Zm0-160v-80h640v80H160Zm0-160v-80h640v80H160Zm0-160v-80h640v80H160Z" />
+                  </svg>
+                  <h3>Description</h3>
+                </div>
+                <button
+                  className="section-action-btn"
+                  onClick={() => setIsEditing(true)}
                 >
-                  <path d="M160-200v-80h400v80H160Zm0-160v-80h640v80H160Zm0-160v-80h640v80H160Zm0-160v-80h640v80H160Z" />
-                </svg>
-                <h3>Description</h3>
+                  Edit
+                </button>
               </div>
               {isEditing ? (
                 <div className="description-edit">
@@ -703,17 +711,33 @@ export function TaskDetails({}) {
             {task.attachments && task.attachments.length > 0 && (
               <div className="task-details-attachments">
                 <div className="attachments-header">
-                  <svg
-                    className="attachments-icon"
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="20px"
-                    viewBox="0 -960 960 960"
-                    width="20px"
-                    fill="#5f6368"
+                  <div className="attachments-header-left">
+                    <svg
+                      className="attachments-icon"
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="20px"
+                      viewBox="0 -960 960 960"
+                      width="20px"
+                      fill="#5f6368"
+                    >
+                      <path d="M720-330q0 104-73 177T470-80q-104 0-177-73t-73-177v-370q0-75 52.5-127.5T400-880q75 0 127.5 52.5T580-700v350q0 46-32 78t-78 32q-46 0-78-32t-32-78v-370h80v370q0 13 8.5 21.5T470-320q13 0 21.5-8.5T500-350v-350q-1-42-29.5-71T400-800q-42 0-71 29t-29 71v370q-1 71 49 120.5T470-160q70 0 119-49.5T640-330v-390h80v390Z" />
+                    </svg>
+                    <h3>Attachments</h3>
+                  </div>
+                  <button
+                    className="section-action-btn"
+                    onClick={(e) => {
+                      const rect = e.currentTarget.getBoundingClientRect()
+                      setAttachmentButtonPosition({
+                        x: rect.left,
+                        y: rect.bottom + 8,
+                      })
+                      setActiveButton('attachment')
+                      setShowAttachmentsModal(true)
+                    }}
                   >
-                    <path d="M720-330q0 104-73 177T470-80q-104 0-177-73t-73-177v-370q0-75 52.5-127.5T400-880q75 0 127.5 52.5T580-700v350q0 46-32 78t-78 32q-46 0-78-32t-32-78v-370h80v370q0 13 8.5 21.5T470-320q13 0 21.5-8.5T500-350v-350q-1-42-29.5-71T400-800q-42 0-71 29t-29 71v370q-1 71 49 120.5T470-160q70 0 119-49.5T640-330v-390h80v390Z" />
-                  </svg>
-                  <h3>Attachments</h3>
+                    Add
+                  </button>
                 </div>
 
                 {/* Links Section */}
