a075ee71f52a4b13d9c9fc09fd83c1f1de295d82
CHEN-BD33 <chenben10.cbd@gmail.com>
2025-06-24 20:32:53 +0300

    Fix name

--- src/cmps/TaskQuickEdit.jsx
+++ src/cmps/TaskQuickEdit.jsx
@@ -207,7 +207,7 @@ export function TaskQuickEdit({
 
                         <button className="action-button">
                             {/* <CopyIcon label="Copy card" color="currentColor" /> */}
-                            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path ffillRule="evenodd" clipRule="evenodd" d="M5 16V4.99188C5 3.8918 5.90195 3 7.00853 3H14.9915L15 3.00002V5H7V16H5ZM8 19C8 20.1046 8.89543 21 10 21H18C19.1046 21 20 20.1046 20 19V8C20 6.89543 19.1046 6 18 6H10C8.89543 6 8 6.89543 8 8V19ZM10 8V19H18V8H10Z" fill="172B4D"></path></svg>
+                            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M5 16V4.99188C5 3.8918 5.90195 3 7.00853 3H14.9915L15 3.00002V5H7V16H5ZM8 19C8 20.1046 8.89543 21 10 21H18C19.1046 21 20 20.1046 20 19V8C20 6.89543 19.1046 6 18 6H10C8.89543 6 8 6.89543 8 8V19ZM10 8V19H18V8H10Z" fill="172B4D"></path></svg>
                             <span>Copy card</span></button>
 
                         <button className="action-button delete-icon" onClick={() => onRemoveTask(group.id, task.id)}><DeleteIcon label="Delete" color="currentColor" /><span>Delete</span></button>
