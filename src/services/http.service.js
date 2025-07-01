ff1e49b2a6b9fecab6ba5ef708f97f4ad76d73c4
Maor <Maoryad@gmail.com>
2025-07-01 23:08:07 +0300

    change http service to get api from the render

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,7 +1,6 @@
 import Axios from 'axios'
 
-const BASE_URL =
-  process.env.NODE_ENV === 'production' ? '/api/' : '//localhost:3031/api/'
+const BASE_URL = import.meta.env.VITE_API_URL || '/api/'
 
 const axios = Axios.create({ withCredentials: true })
 
